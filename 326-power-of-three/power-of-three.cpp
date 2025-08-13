77eff2d146afcf79dbe13bcb5c1d011d49262bfc
Suhani yadav <155080026+QuantumSuhani@users.noreply.github.com>
2025-08-14 00:07:57 +0530

    Time: 4 ms (47.44%) | Memory: 8.8 MB (76.82%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0){
+        if (n <= 0) {
+            return false;  // Negative numbers and zero are not powers of three
+        }
+
+        while (n % 3 == 0) {
+            n = n / 3;  // Keep dividing by 3
+        }
+
+        // After the loop, check if n became 1
+        if (n == 1) {
+            return true;
+        } 
+        else {
             return false;
         }
-         while(n%3==0){
-            n=n/3;
-         }
-         return n==1;
-        
     }
 };
