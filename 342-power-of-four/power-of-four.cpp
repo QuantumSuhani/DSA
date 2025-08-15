e0b41d9161e1e44ad397f4b96fa8ae5021f8b420
Suhani yadav <155080026+QuantumSuhani@users.noreply.github.com>
2025-08-15 12:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.14%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -5,9 +5,13 @@ public:
         if(n==0){
             return false;
         }
-        while(n%4==0&&n>0){
+        while(n%4==0 && n>0){
             n=n/4;
         }
-           return n==1;
+           if(n!=1){
+            return false;
+           }
+           
+    return 1;
     }
 };
