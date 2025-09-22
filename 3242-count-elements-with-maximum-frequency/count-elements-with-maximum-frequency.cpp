75588fa7151f9bbf9bbc66f8342c0bd52ae552ca
Suhani yadav <155080026+QuantumSuhani@users.noreply.github.com>
2025-09-22 23:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (67.03%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -3,17 +3,20 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         vector<int>count(101);
         int maxfreq=0;
-    for(int &num:nums){
-        count[num]++;
-        maxfreq=max(maxfreq,count[num]);
-    }
-    int result=0;
-    for(int i=0;i<101;i++){
-        if(count[i]==maxfreq){
-            result=result+maxfreq;
+        int total=0;
+        for(int &num:nums){
+            count[num]++;
+
+            int freq=count[num];
+            if(freq>maxfreq){
+                 maxfreq=freq;
+                 total=maxfreq;
+            }
+            else if(freq==maxfreq){
+                total=total+maxfreq;
+
+            }
         }
-    }
-    return result;
-        
-    }
-};
+            return total;
+        }
+    };
